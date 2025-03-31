c5f7cc9a6f70ece76e5f084e4d5c405f3184cea3
wraithio <aarmon5@gmail.com>
2025-03-31 15:28:43 -0700

    trying create account again

--- obj/Release/net8.0/sheargenius-backend.AssemblyInfo.cs
+++ obj/Release/net8.0/sheargenius-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484e4c8b3103914054db23fbdaf7e408fbb496dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5538f7fed7307199cfd61bff6203bfd8d3a95c38")]
 [assembly: System.Reflection.AssemblyProductAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
