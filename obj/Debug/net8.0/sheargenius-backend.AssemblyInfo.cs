46c46a129755fb30117f47c3d40b2653e49f474c
DPattonINIT <DlamarPatton@gmail.com>
2025-04-25 16:15:44 -0700

    merging files

--- obj/Debug/net8.0/sheargenius-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/sheargenius-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ba666b0051fee250777856ec842ae6f4a2886d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5eb30a560ff8b84ad13a8d6ab463c4aa5e0d461")]
 [assembly: System.Reflection.AssemblyProductAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
