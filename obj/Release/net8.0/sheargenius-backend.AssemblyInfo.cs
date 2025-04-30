5d49c423c8cab4dc3aade0169a0961b7a8ecb7ce
wraithio <aarmon5@gmail.com>
2025-04-30 15:45:47 -0700

    rating depoly

--- obj/Release/net8.0/sheargenius-backend.AssemblyInfo.cs
+++ obj/Release/net8.0/sheargenius-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3e56c93bbe713cb3680677668a375f6d1c1728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa16dd573061618f22d1e9cde2360daa91e1da06")]
 [assembly: System.Reflection.AssemblyProductAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
