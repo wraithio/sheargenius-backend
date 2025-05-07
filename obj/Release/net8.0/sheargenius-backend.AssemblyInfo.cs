2575f4fd5d07b24db9809d452ebffc942a896533
wraithio <aarmon5@gmail.com>
2025-05-07 14:10:13 -0700

    deployed

--- obj/Release/net8.0/sheargenius-backend.AssemblyInfo.cs
+++ obj/Release/net8.0/sheargenius-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d49c423c8cab4dc3aade0169a0961b7a8ecb7ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac73a195aaafd7ef94320a46fa23e84c306e7023")]
 [assembly: System.Reflection.AssemblyProductAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sheargenius-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
